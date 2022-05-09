c9ae1e071bb04db67da606bf512772857e198254
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-05-09 20:05:38 +0200

    setup layouts and configure constraints

--- TwitterClone/TwitterClone/Controllers/AddTweetViewController.swift
+++ TwitterClone/TwitterClone/Controllers/AddTweetViewController.swift
@@ -9,21 +9,77 @@ import UIKit
 
 class AddTweetViewController: UIViewController {
 
+    
+    //MARK:- Vars
+    private let  profileImage : UIImageView = {
+       let image = UIImageView()
+        image.layer.cornerRadius = 25
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.backgroundColor = .green
+        
+        return image
+    }()
+    
+    private let tweetButton : UIButton = {
+       let button = UIButton()
+        button.setTitle("Tweet", for: .normal)
+        button.backgroundColor = .blue
+        button.layer.cornerRadius = 15
+        
+    
+        return button
+    }()
+    
+    private let tweetBoxField : UITextField = {
+        let text = UITextField()
+        text.placeholder = "Whats happning now?"
+        text.borderStyle = .none
+ 
+        return text
+    }()
+    
+    //MARK:- Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Tweet", style: .done, target: self, action: #selector(didTapTweetButton))
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    //MARK:- Layouts and Constraints
+    private func setupLayouts (){
+        view.addSubview(profileImage)
+        view.addSubview(tweetBoxField)
     }
-    */
+    
+    private func configureCosntraints(){
+        NSLayoutConstraint.activate([
+            
+            profileImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            profileImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            profileImage.heightAnchor.constraint(equalToConstant: 50),
+            profileImage.widthAnchor.constraint(equalToConstant: 50),
+            
+            tweetBoxField.leadingAnchor.constraint(equalTo: profileImage.trailingAnchor, constant: 10),
+            tweetBoxField.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tweetBoxField.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            
+        
+            
+            
+        ])
+    }
+    
+    
+    
+    //MARK:- Tweet Button
+    @objc private func didTapTweetButton() {
+        
+    }
+    
+    
+    
 
 }
