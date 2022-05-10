fd56bbf39be1a9c4524eb302a6f21fe2d4ea5de2
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-05-10 23:16:00 +0200

    Customize Navigation bar for MessagesViewController

--- TwitterClone/TwitterClone/Controllers/Core/DirectMessagesViewController.swift
+++ TwitterClone/TwitterClone/Controllers/Core/DirectMessagesViewController.swift
@@ -14,8 +14,35 @@ class DirectMessagesViewController: UIViewController {
 
         
         view.backgroundColor = .brown
+        
+        configureNavBar()
     }
     
+    
+    
+    func configureNavBar()  {
+        
+        // set the logo in the center
+        title = "Messages"
+        
+       
+        
+        // set profile image in the left
+        let containerView = UIControl(frame: CGRect.init(x: 0, y: 0, width: 30, height: 30))
+        let image = UIImageView(frame: CGRect.init(x: 0, y: 0, width: 30, height: 30))
+        image.backgroundColor = .systemBlue
+        image.layer.cornerRadius = image.layer.frame.size.width / 2
+        containerView.addSubview(image)
+        let profileImage = UIBarButtonItem(customView: containerView)
+        profileImage.width = 20
+        navigationItem.leftBarButtonItem = profileImage
+        
+        // set the right buton to the navBar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .done, target: self, action: nil)
+        
+        
+        navigationController?.navigationBar.tintColor = .systemBlue
+    }
 
     /*
     // MARK: - Navigation
