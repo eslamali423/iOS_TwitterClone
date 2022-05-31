58fc58543ec9f51839eb1038ff0ce83fd0ac8464
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-05-31 18:51:12 +0200

    Create AuthManager and create LoginFunction To Firebase

---
+++ TwitterClone/TwitterClone/Managers/AuthManager.swift
@@ -0,0 +1,30 @@
+//
+//  AuthManager.swift
+//  TwitterClone
+//
+//  Created by Eslam Ali  on 31/05/2022.
+//
+
+import Foundation
+import Firebase
+
+class AuthManager {
+    
+    static let shared =  AuthManager()
+    
+    //MARK:- login function with email and password
+    func login(email:String, password:String, completion: @escaping (_ error: Error?, _ isEmailVerified : Bool )->Void ) {
+        Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+            if error == nil && authResult!.user.isEmailVerified  {
+            completion(error,true)
+                self.downloadUserFormFirestore(userID: authResult!.user.uid)
+
+            }else {
+                print("something went worng by login ")
+                   print(error?.localizedDescription)
+            completion(error,false)
+            }
+        }
+    }
+    
+}
