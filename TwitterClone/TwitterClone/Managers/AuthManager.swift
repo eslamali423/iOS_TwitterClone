8b5f7db3b79ca798a228b5b8fc31dccddc71261d
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-06-02 15:43:45 +0200

    Create new user in Firebase

--- TwitterClone/TwitterClone/Managers/AuthManager.swift
+++ TwitterClone/TwitterClone/Managers/AuthManager.swift
@@ -12,6 +12,29 @@ class AuthManager {
     
     static let shared =  AuthManager()
     
+  
+    //MARK:- registration function with email and password
+    func registration(email : String, password :  String, completion: @escaping (_ error : Error?)-> Void )  {
+        Auth.auth().createUser(withEmail: email, password: password) { (results, error) in
+         
+            completion(error)
+            
+            if error == nil {
+            results?.user.sendEmailVerification(completion: { (error) in
+                print(error?.localizedDescription)
+            
+            })
+            
+            if results?.user != nil {
+               // let user =  User(id: results!.user.uid, username: email, email: email, pushID: "", avatarLink: "", status: "hey i'm using this chat App ")
+//self.saveUserToFirestore(user: user)
+               // saveUserLocally(user)
+            }
+        }
+    }
+    }
+    
+    
     //MARK:- login function with email and password
     func login(email:String, password:String, completion: @escaping (_ error: Error?, _ isEmailVerified : Bool )->Void ) {
         Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
