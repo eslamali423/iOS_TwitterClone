ded4bdb749c3d238c589acd71dc2a285b578e816
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-06-04 17:13:35 +0200

    Create User Model

---
+++ TwitterClone/TwitterClone/Models/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  TwitterClone
+//
+//  Created by Eslam Ali  on 04/06/2022.
+//
+
+import Foundation
+
+struct User : Codable {
+    let id : String
+    let name : String
+    let username : String
+    let email : String
+    let bio : String?
+   
+}
